e8719c4b861cdfec71e28e7ec72294a1908837d9
eatthischair <based.god.swag.od@gmail.com>
2025-03-11 17:14:27 -0500

    finished theme provider

--- src/app/ui/Theme/Theme.tsx
+++ src/app/ui/Theme/Theme.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { theme } from '../theme';
 
+//random comment for git commit
 export const Theme = ({ children }: any) => {
   const nightMode = useSelector((state) => state.themeReducer.nightMode);
   const classNameString = nightMode
