009289b730884e4c423645e02eb90a42a23bf2be
eatthischair <based.god.swag.od@gmail.com>
2025-06-24 17:59:40 -0500

    testing

--- src/app/ui/HeaderComponents/index.tsx
+++ src/app/ui/HeaderComponents/index.tsx
@@ -4,20 +4,27 @@ import { DataBar } from './MarketData/DataBar';
 import { useQuery } from '@tanstack/react-query';
 import Skeleton from 'react-loading-skeleton';
 import { getGlobalData } from '@/app/api/getGlobalData';
-
+import { useState } from 'react';
 export function HeaderComponents() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['dataBar'],
-    queryFn: async () => {
-      const response = await fetch('https://api.coingecko.com/api/v3/global');
-      return await response.json();
-    },
-  });
+  // const { isPending, error, data } = useQuery({
+  //   queryKey: ['dataBar'],
+  //   queryFn: async () => {
+  //     const response = await fetch('https://api.coingecko.com/api/v3/global');
+  //     return await response.json();
+  //   },
+  // });
 
+  const [data, setData] = useState({});
+  const fetchStuff = async () => {
+    const data = await getGlobalData();
+    setData(data);
+  };
+  fetchStuff();
   return (
     <>
       <NavBar />
-      {isPending || error ? <Skeleton /> : <DataBar data={data.data} />}
+      {!data ? <Skeleton /> : <DataBar data={data.data} />}
+      {/* <DataBar data={data?.data} /> */}
     </>
   );
 }
