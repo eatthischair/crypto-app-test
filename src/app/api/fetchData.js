1a6e4da8402be9417ae7445e4547e68bd2bfc8f4
eatthischair <based.god.swag.od@gmail.com>
2025-06-24 15:39:37 -0500

    hardcoding api key

--- src/app/api/fetchData.js
+++ src/app/api/fetchData.js
@@ -1,15 +1,16 @@
 'use server';
 
 export const fetchData = async (url) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('API KEY:', process.env.COINGECKO_API_KEY);
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   console.log('API KEY:', process.env.COINGECKO_API_KEY);
+  // }
 
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      'x-cg-demo-api-key': process.env.COINGECKO_API_KEY,
+      // 'x-cg-demo-api-key': process.env.COINGECKO_API_KEY,
+      'x-cg-demo-api-key': CG - tLCRhygcvpcYho3BrWGp8J7m,
     },
     // cache: 'force-cache',
     next: { revalidate: 3600 },
