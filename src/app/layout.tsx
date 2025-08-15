fa3252b859b3a4132c6e2e9474a9e1d545cd2629
eatthischair <based.god.swag.od@gmail.com>
2025-08-15 16:10:35 -0500

    fixing right margin again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <div className="sm:w-full sm:h-full sm:p-12 sm:m-0 -mr-2">
+        <div className="sm:w-full sm:h-full sm:p-12 sm:m-0 -mr-12 border">
           <QueryClientProvider client={queryClient}>
             <Provider store={store}>
               <ThemeProvider
