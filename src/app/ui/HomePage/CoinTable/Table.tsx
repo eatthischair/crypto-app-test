c38f5637c9984220fa3a68d816400e26284c2a70
eatthischair <based.god.swag.od@gmail.com>
2025-08-21 16:20:04 -0500

    iiiii

--- src/app/ui/HomePage/CoinTable/Table.tsx
+++ src/app/ui/HomePage/CoinTable/Table.tsx
@@ -22,7 +22,7 @@ export const Table = ({ coinsSorted, getData, hasMore }) => {
         }
         scrollThreshold={1}
         endMessage={<p style={{ textAlign: 'center' }}>The end</p>}
-        className="grid auto-cols-auto overscroll-none sm:p-0 p-3 gap-3 !overflow-visible touch-auto"
+        className="grid auto-cols-auto overscroll-none sm:p-0 p-3 gap-3  touch-auto pb-16"
       >
         {coinsSorted.map((row, index) => {
           return <TableRow coin={row} index={index} key={row.id} />;
